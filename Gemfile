f205a7d5d6bdacee90b5535cab09b8dd0afe46cd
Edmund Dipple <elmundio1987@gmail.com>
2013-12-16 17:27:04 +0000

    pull information out of a chef server! a bit slow but it works

--- Gemfile
+++ Gemfile
@@ -43,3 +43,6 @@ end
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+
+# Use for chef API calls
+gem 'ridley'
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,38 @@
 class DashboardController < ApplicationController
 
+require 'ridley'
 
 def index
-  @Vms = Vm.all
+
+    ridley = Ridley.new(
+      server_url: "https://api.opscode.com/organizations/elmundio87",
+      client_name: "elmundio87-validator",
+      client_key: "/Users/edmundd/.chef/elmundio87-validator.pem"
+    )
+
+    @Vms = Array.new
+
+
+    (ridley.node.all).each do |node|
+
+        hostname = node.chef_attributes.hostname
+
+        node_reloaded = node.reload
+
+        vm = Hash.new
+        vm["name"] = node_reloaded.chef_attributes.hostname
+        vm["os"] = node_reloaded.chef_attributes.kernel.name
+        vm["owner"] = "???"
+        vm["ip_address"] = node_reloaded.chef_attributes.ipaddress
+        vm["host_type"] = "???"
+        vm["description"] = "???"
+        @Vms << vm
+
+    end
+
+
+
 end
+
 
 end
