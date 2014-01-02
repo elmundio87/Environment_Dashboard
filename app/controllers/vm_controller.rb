d5aedffe4d8f418e922805662650cf289baade35
Edmund Dipple <elmundio1987@gmail.com>
2014-01-02 12:50:31 +0000

    move includes to correct class

--- app/controllers/vm_controller.rb
+++ app/controllers/vm_controller.rb
@@ -1,19 +1,7 @@
 class VmController < ApplicationController
-  def stats
 
-        ridley = RidleySingleton.instance.getConnection();
-
-        node_reloaded = ridley.node.all[0].reload.chef_attributes
-
-        vm = Hash.new
-        vm["name"] = node_reloaded.hostname
-        vm["cpu_speed"] = node_reloaded.cpu[0].mhz
-        vm["cpu_cores"] = node_reloaded.cpu[0].cores
-        vm["ram"] = node_reloaded.kernel.os_info.total_visible_memory_size
-
-
-    render :xml => vm
-  end
+require 'ridley'
+require 'RidleySingleton'
 
   def roster
 
@@ -69,7 +57,7 @@ class VmController < ApplicationController
     end
 
     def screenshot
-        send_data get_screenshot("127.0.0.1"), :type => 'image/png'
+        send_data get_screenshot(params[:ipAddress]), :type => 'image/png'
     end
 
  def get_screenshot(ip)
