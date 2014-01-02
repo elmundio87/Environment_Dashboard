656e0ce5e077a2e674f8a3a09e97e1278096bdbb
Edmund Dipple <elmundio1987@gmail.com>
2014-01-02 15:19:12 +0000

    actually make the multithreaded server work!

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ EnvironmentManagerDashboard::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.threadsafe!
 end
